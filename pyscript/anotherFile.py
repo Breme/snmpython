d20b0ef0ad6650a1dbb16e0301ca0b425960078c
Breme <breme.arun07@gmail.com>
2018-04-09 06:47:14 +0530

    Add files via upload

---
+++ pyscript/anotherFile.py
@@ -0,0 +1,9 @@
+print 'this is Breme'
+
+def test(name,email):
+    print 'inside methos'
+    return 'testSuccess'
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+   print 'main'
